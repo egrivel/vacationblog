ab8aee4c386e291fae3cb615fefa4c8440658691
egrivel <github-com@lumenssolutions.com>
2016-01-31 12:47:49 -0500

    Prompt for info to initialize database

--- site/api/CreateTables.php
+++ site/api/CreateTables.php
@@ -12,27 +12,143 @@ include_once("../database/User.php");
 
 $setting = new Setting();
 $version = $setting->getDataVersion();
-Auth::updateTables($version);
-Comment::updateTables($version);
-Feedback::updateTables($version);
-Journal::updateTables($version);
-Media::updateTables($version);
-Setting::updateTables($version);
-Trip::updateTables($version);
-TripAttribute::updateTables($version);
-TripUser::updateTables($version);
-User::updateTables($version);
-if ($version === "") {
-   $config = parse_ini_file('./defaultUser.ini');
-   $user = new User($config['username']);
-   if ($user->getName() === "") {
-      // default user "eric" doesn't exist
-      print "<p>Initialize user $user</p>\n";
-      $user->setName($config['fullname']);
-      $user->setPassword($config['password']);
-      $user->setAccess("admin");
-      $user->setEmail($config['email']);
-      $user->save();
+
+function updateTables($version) {
+   Auth::updateTables($version);
+   Comment::updateTables($version);
+   Feedback::updateTables($version);
+   Journal::updateTables($version);
+   Media::updateTables($version);
+   Setting::updateTables($version);
+   Trip::updateTables($version);
+   TripAttribute::updateTables($version);
+   TripUser::updateTables($version);
+   User::updateTables($version);
+   print "Tables have been updated. ";
+}
+
+function validateUser() {
+   $name = $_POST['name'];
+   $fullname = $_POST['fullname'];
+   $password = $_POST['password'];
+   $password2 = $_POST['password2'];
+   $email = $_POST['email'];
+
+   $errors = '';
+   if (!$name) {
+      $errors .= "<li>Please provide a username</li>\n";
    }
- }
+
+   if (!$fullname) {
+      $errors .= "<li>Please provide a full name</li>\n";
+   }
+
+   if (!$password) {
+      $errors .= "<li>Please provide a password</li>\n";
+   } else if ($password2 !== $password) {
+      $errors .= "<li>Passwords do not match</li>\n";
+   }
+
+   if (!$fullname) {
+      $errors .= "<li>Please provide an email address</li>\n";
+   }
+
+   return $errors;
+}
+
+if ($version === "") {
+   // There are no tables yet, so tables must be created. To create,
+   // we need to have the initial user information.
+
+   $name = $_POST['name'];
+   $fullname = $_POST['fullname'];
+   $password = $_POST['password'];
+   $password2 = $_POST['password2'];
+   $email = $_POST['email'];
+   $do = $_POST['do'];
+
+   if ($do) {
+      // got user input, check it
+      $errors = validateUser();
+      if ($errors) {
+         showForm($errors);
+      } else {
+         updateTables($version);
+         $user = new User($name);
+         $user->setName($fullname);
+         $user->setPassword($password);
+         $user->setAccess('admin');
+         $user->setEmail($email);
+         $user->save();
+      }
+   } else {
+      showForm('');
+   }
+} else {
+   updateTables($version);
+}
+
+function showForm($errors) {
+   $name = $_POST['name'];
+   $fullname = $_POST['fullname'];
+   $password = $_POST['password'];
+   $password2 = $_POST['password2'];
+   $email = $_POST['email'];
+
+   print "<!DOCTYPE>
+<html>
+  <head>
+    <title>Initialize Database</title>
+  </head>
+  <body>
+    <h1>Initialize Database</h1>
+    <p>This page is displayed because the database for the Vacation Blog
+      has not yet been initialized. Please provide the information on this
+      page to initialize the database.</p>\n";
+
+   if ($errors) {
+      print "<ul style='color: red'>$errors</ul>\n";
+   }
+
+   print "
+    <form method='POST' action='CreateTables.php'>
+      <p>
+        <label for='name'>User Name:</label>
+        <input type='text' name='name' id='name' value='$name' size='32' maxlength='32'/>
+      </p>
+
+      <p>
+        <label for='fullname'>Full Name:</label>
+        <input type='text' name='fullname' id='fullname' value='$fullname' size='32' maxlength='64'/>
+      </p>
+
+      <p>
+        <label for='password'>Password:</label>
+        <input type='password' name='password' id='password' value='$password' size='32' maxlength='32'/>
+      </p>
+
+      <p>
+        <label for='password2'>Repeat Password:</label>
+        <input type='password' name='password2' id='password2' value='$password2' size='32' maxlength='32'/>
+      </p>
+
+      <p>
+        <label for='email'>Email:</label>
+        <input type='text' name='email' id='email' value='$email' size='32' maxlength='64'/>
+      </p>
+
+      <p>
+        <input type='submit' name='do' id='do' value='Initialize Database'/>
+        <input type='submit' name='cancel' id='cancel' value='Cancel'/>
+    </form>
+  </body>
+</html>
+";
+
+   print "<!DOCTYPE html\n";
+   print "<html>\n";
+   exit(0);
+}
+
 ?>
+OK
