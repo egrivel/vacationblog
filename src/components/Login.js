a9c793c03cf5bd8918c75fbc6d558183a9502383
egrivel <github-com@lumenssolutions.com>
2016-01-31 18:32:28 -0500

    Built basic login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,9 +8,16 @@ var Login = React.createClass({
   render: function() {
     return React.DOM.div(
       {
-        className: 'form'
+        className: 'form login'
       },
-      'This is the login window.',
+      React.DOM.h3(
+        null,
+        'Login'
+      ),
+      React.DOM.p(
+        null,
+        'Please enter your user name and password to log into the site.'
+      ),
       React.DOM.div(
         { className: 'input-element' },
         React.DOM.label(
@@ -19,8 +26,40 @@ var Login = React.createClass({
         ),
         React.DOM.input(
           {
+            type: 'text',
             name: 'username',
             id: 'username'
+          }
+        )
+      ),
+      React.DOM.div(
+        { className: 'input-element' },
+        React.DOM.label(
+          null,
+          'Password:'
+        ),
+        React.DOM.input(
+          {
+            type: 'password',
+            name: 'username',
+            id: 'username'
+          }
+        )
+      ),
+
+      React.DOM.div(
+        { className: 'buttonbar' },
+        React.DOM.input(
+          {
+            type: 'submit',
+            value: 'Login'
+          }
+        ),
+
+        React.DOM.input(
+          {
+            type: 'submit',
+            value: 'Cancel'
           }
         )
       )
