e55be342ff319ca16f96d877e1a5ef25ec0cf44e
Eric Grivel <github-com@lumenssolutions.com>
2016-02-15 20:46:17 -0500

    Extracted journal entry prev next to its own component

--- src/components/JournalEntry.js
+++ src/components/JournalEntry.js
@@ -19,208 +19,26 @@
 /* global window */
 
 var React = require('react');
-var Link = require('react-router').Link;
 
 var JournalStore = require('../stores/JournalStore');
 var MediaStore = require('../stores/MediaStore');
 var UserStore = require('../stores/UserStore');
 var CommentStore = require('../stores/CommentStore');
+var storeMixin = require('./StoreMixin');
 
 var Paragraph = require('./Paragraph');
 var CommentList = require('./CommentList');
 var Feedback = require('./Feedback');
 var JournalEntryHeader = require('./JournalEntryHeader.jsx');
+var JournalEntryPrevNext = require('./JournalEntryPrevNext.jsx');
 var utils = require('./utils');
-
-/**
- * Get the state from the stores.
- * @return {object} new state.
- * @private
- */
-function _getStateFromStores() {
-  var journalData = JournalStore.getData();
-  var userName = '';
-  var comments = null;
-
-  if (!journalData || !journalData.tripId) {
-    // There is no actual journal item. Clear out the state.
-    return {
-      tripId: null,
-      journalId: null,
-      journalTitle: 'Not Found',
-      journalText: 'The requested journal item was not found.',
-      journalDate: null,
-      created: null,
-      userId: null,
-      userName: null,
-      nextId: null,
-      prevId: null,
-      comments: null
-    };
-  }
-
-  if (journalData && journalData.userId) {
-    var userData = UserStore.getData(journalData.userId);
-    if (userData) {
-      userName = userData.name;
-    } else {
-      // If no user name can be retrieved, default to the user ID.
-      userName = journalData.userId;
-    }
-  }
-
-  if (journalData.tripId && journalData.journalId) {
-    comments = CommentStore.getRecursiveData(journalData.tripId,
-                                             journalData.journalId);
-  }
-
-  return {
-    tripId: journalData.tripId,
-    journalId: journalData.journalId,
-    journalTitle: journalData.journalTitle,
-    journalText: journalData.journalText,
-    journalDate: journalData.journalDate,
-    created: journalData.created,
-    userId: journalData.userId,
-    userName: userName,
-    nextId: journalData.nextId,
-    prevId: journalData.prevId,
-    comments: comments
-  };
-}
-
-/**
- * Build a 'previous' or 'next' link.
- * @param {id} tripId - unique trip ID.
- * @param {id} targetId - unique ID of the target journal entry.
- * @param {string} icon - name of the icon to use in the link.
- * @param {string} className - class name to add to the link.
- * @param {string} label - label for the link.
- * @param {string} defaultLabel - label to use instead if there is no link.
- * @return {object} React element representing the previous or next link.
- * @private
- */
-function _getPrevNextPart(tripId, targetId, icon, className,
-                          label, defaultLabel) {
-  var icon1 = null;
-  var icon2 = null;
-  if (className === 'prevlink') {
-    icon1 = React.DOM.i(
-      {
-        className: 'fa ' + icon
-      }
-    );
-  } else {
-    icon2 = React.DOM.i(
-      {
-        className: 'fa ' + icon
-      }
-    );
-  }
-  if (targetId) {
-    return React.DOM.span(
-      {
-        className: className
-      },
-      React.createElement(
-        Link,
-        {
-          to: '/journal/' + tripId + '/' + targetId
-        },
-        icon1, icon1,
-        ' ' + label + ' ',
-        icon2, icon2
-      )
-    );
-  }
-  return React.DOM.span(
-    {
-      className: className
-    },
-    defaultLabel
-  );
-}
-
-/**
- * Get the prev/next line for a journal entry.
- * @param {id} tripId - unique trip ID.
- * @param {id} prevId - ID of the previous journal entry.
- * @param {id} nextId - ID of the next journal entry.
- * @param {int} nr - instance number in the parent; there are two
- * prev/next lines on display, which need to get a unique React key.
- * @return {object} React element for the prev/next line.
- * @private
- */
-function _getPrevNextLine(tripId, prevId, nextId, nr) {
-  if (!tripId) {
-    return null;
-  }
-  var prevPart =
-    _getPrevNextPart(tripId, prevId, 'fa-chevron-left', 'prevlink',
-                     'Previous Post', '(no previous post)');
-  var nextPart =
-    _getPrevNextPart(tripId, nextId, 'fa-chevron-right', 'nextlink',
-                     'Next Post', '(no next post)');
-  return React.DOM.p(
-    {
-      key: 'prevnext-' + nr,
-      className: 'prevnext'
-    },
-    prevPart, ' \u2022 ', nextPart);
-}
-
-/**
- * Get the header for the journal entry.
- * @param {string} journalTitle - title of the entry.
- * @param {date} journalDate - date for the entry.
- * @param {id} userId - user ID of the user who created the entry.
- * @param {string} userName - formatted name of the user who created the
- * entry.
- * @param {date} created - timestamp when entry was created.
- * @return {object} React element for journal entry header.
- * @private
- */
-function _getHeader(journalTitle, journalDate, userId, userName, created) {
-  return React.createElement(
-    JournalEntryHeader,
-    {
-      title: journalTitle,
-      date: journalDate,
-      userName: userName,
-      created: created
-    }
-  );
-}
 
 var JournalEntry = React.createClass({
   displayName: 'JournalEntry',
 
-  getInitialState: function() {
-    return _getStateFromStores();
-  },
+  stores: [JournalStore, MediaStore, UserStore, CommentStore],
 
-  _onChange: function() {
-    this.setState(_getStateFromStores());
-  },
-
-  /**
-   * React lifecycle function. Called when the component is first
-   * mounted. Not called on updates; the componentWillReceiveProps is
-   * called on updates.
-   */
-  componentDidMount: function() {
-    JournalStore.addChangeListener(this._onChange);
-    MediaStore.addChangeListener(this._onChange);
-    UserStore.addChangeListener(this._onChange);
-    CommentStore.addChangeListener(this._onChange);
-  },
-
-  componentWillUnmount: function() {
-    CommentStore.removeChangeListener(this._onChange);
-    UserStore.removeChangeListener(this._onChange);
-    MediaStore.removeChangeListener(this._onChange);
-    JournalStore.removeChangeListener(this._onChange);
-  },
+  mixins: [storeMixin()],
 
   /**
    * React lifecycle function. Called when the components is already
@@ -236,6 +54,61 @@ var JournalEntry = React.createClass({
     window.scrollTo(0, 0);
   },
   /* eslint-enable no-unused-vars */
+
+  /**
+   * Get the state from the stores.
+   * @return {object} new state.
+   * @private
+   */
+  _getStateFromStores: function() {
+    var journalData = JournalStore.getData();
+    var userName = '';
+    var comments = null;
+
+    if (!journalData || !journalData.tripId) {
+      // There is no actual journal item. Clear out the state.
+      return {
+        tripId: null,
+        journalId: null,
+        journalTitle: 'Not Found',
+        journalText: 'The requested journal item was not found.',
+        journalDate: null,
+        created: null,
+        userName: null,
+        nextId: null,
+        prevId: null,
+        comments: null
+      };
+    }
+
+    if (journalData && journalData.userId) {
+      var userData = UserStore.getData(journalData.userId);
+      if (userData) {
+        userName = userData.name;
+      } else {
+        // If no user name can be retrieved, default to the user ID.
+        userName = journalData.userId;
+      }
+    }
+
+    if (journalData.tripId && journalData.journalId) {
+      comments = CommentStore.getRecursiveData(journalData.tripId,
+                                               journalData.journalId);
+    }
+
+    return {
+      tripId: journalData.tripId,
+      journalId: journalData.journalId,
+      journalTitle: journalData.journalTitle,
+      journalText: journalData.journalText,
+      journalDate: journalData.journalDate,
+      created: journalData.created,
+      userName: userName,
+      nextId: journalData.nextId,
+      prevId: journalData.prevId,
+      comments: comments
+    };
+  },
 
   render: function render() {
     var parCount = 0;
@@ -262,15 +135,23 @@ var JournalEntry = React.createClass({
       {
         className: 'journalitem'
       },
-      _getHeader(this.state.journalTitle,
-                 this.state.journalDate,
-                 this.state.userId,
-                 this.state.userName,
-                 this.state.created),
-      _getPrevNextLine(this.state.tripId,
-                       this.state.prevId,
-                       this.state.nextId,
-                       1),
+      React.createElement(
+        JournalEntryHeader,
+        {
+          title: this.state.journalTitle,
+          date: this.state.journalDate,
+          userName: this.state.userName,
+          created: this.state.created
+        }
+      ),
+      React.createElement(JournalEntryPrevNext,
+        {
+          tripId: tripId,
+          prevId: this.state.prevId,
+          nextId: this.state.nextId,
+          nr: 1
+        }
+      ),
       parList.map(function(par) {
         parCount++;
         var parKey = 'p-' + parCount;
@@ -282,10 +163,14 @@ var JournalEntry = React.createClass({
       }),
       feedback,
       comment,
-      _getPrevNextLine(this.state.tripId,
-                       this.state.prevId,
-                       this.state.nextId,
-                       2)
+      React.createElement(JournalEntryPrevNext,
+        {
+          tripId: tripId,
+          prevId: this.state.prevId,
+          nextId: this.state.nextId,
+          nr: 2
+        }
+      )
     );
   }
 });
