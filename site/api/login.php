79466327b88b9a371b658c72be5fe59c70efa1eb
Eric Grivel <github-com@lumenssolutions.com>
2016-05-01 09:50:29 -0400

    start of login

---
+++ site/api/login.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * call to log into the system
+ */
+
+include_once(dirname(__FILE__) . "/../common/common.php");
+include_once(dirname(__FILE__) . '/../business/AuthB.php');
+include_once(dirname(__FILE__) . '/../database/Auth.php');
+include_once(dirname(__FILE__) . '/../database/User.php');
+
+if (isPutMethod()) {
+   $data = json_decode(file_get_contents('php://input'), true);
+   $userId = '';
+   if (isset($data['userId'])) {
+      $userId = $data['userId'];
+   }
+   $password = '';
+   if (isset($data['password'])) {
+      $password = $data['password'];
+   }
+   if (($userId === '') || ($password === '')) {
+      $response = errorResponse(RESPONSE_BAD_REQUEST);
+   } else {
+      $user = new User($userId);
+      if ($user->getCreated() === null) {
+        $response = errorResponse(RESPONSE_NOT_FOUND);
+      } else {
+        if ($user->checkPassword($password)) {
+          // password matches, need to allow for hash update (if hash
+          // algorithm changed, the new hash can only be stored in the
+          // database when we have the actual password, and only at login
+          // time do we have the actual password)
+          $user->updatePasswordHash($password);
+          $authId = Auth::generateAuthId();
+          $auth = new Auth($authId);
+          $auth->setUserId($userId);
+          $auth->save();
+          $response = successResponse();
+          $response['authId'] = $authId;
+        } else {
+          $response = errorResponse(RESPONSE_UNAUTHORIZED);
+        }
+      }
+   }
+}
+
+echo json_encode($response);
+?>
---
+++ unittest/api/LoginApiTest.php
@@ -0,0 +1,113 @@
+<?php
+include(dirname(__FILE__) . "/../common.php");
+include_once("$gl_site_root/database/User.php");
+
+class LoginApiTest extends PHPUnit_Framework_Testcase {
+  private $testUserId;
+  private $testPassword;
+
+  public static function setUpBeforeClass() {
+    global $testUserId, $testPassword;
+    $testUserId = '-test-user-1';
+    $testPassword = '--test-password-1--';
+
+    // Cleanup before starting the test
+    // Delete any records from the Auth table for the test user
+    $query = "DELETE FROM blogAuth"
+      . " WHERE userId='$testUserId'";
+    mysql_query($query);
+
+    // Delete any test users
+    $query = "DELETE FROM blogUser"
+      . " WHERE userId='$testUserId'";
+    mysql_query($query);
+
+    // create the test user
+    $object = new User($testUserId);
+    $object->setPassword($testPassword);
+    $object->save();
+  }
+
+  public static function tearDownAfterClass() {
+    global $testUserId;
+
+    // Cleanup after testing
+    // Delete any records from the Auth table for the test user
+    $query = "DELETE FROM blogAuth"
+      . " WHERE userId='$testUserId'";
+    mysql_query($query);
+
+    // Delete any test users
+    $query = "DELETE FROM blogUser"
+      . " WHERE userId='$testUserId'";
+    mysql_query($query);
+  }
+
+  // Login without parameters fails
+  public function testLoginNoParameter() {
+    $result = putApi('login.php', null);
+    $this->assertEquals(RESPONSE_BAD_REQUEST, $result['resultCode']);
+
+    $data = array();
+    $result = putApi('login.php', $data);
+    $this->assertEquals(RESPONSE_BAD_REQUEST, $result['resultCode']);
+  }
+
+  // Login with just user ID fails
+  public function testUserOnlyParameter() {
+    global $testUserId;
+
+    $data = array('userId'=>$testUserId);
+    $result = putApi('login.php', $data);
+    $this->assertEquals(RESPONSE_BAD_REQUEST, $result['resultCode']);
+  }
+
+  // Login with just password fails
+  public function testPasswordOnlyParameter() {
+    global $testPassword;
+
+    $data = array('password'=>$testPassword);
+    $result = putApi('login.php', $data);
+    $this->assertEquals(RESPONSE_BAD_REQUEST, $result['resultCode']);
+  }
+
+  // Login with wrong user ID and password fails
+  public function testWrongUser() {
+    $data = array('userId'=>'-test-wrong-user',
+                  'password'=>'foo');
+    $result = putApi('login.php', $data);
+    $this->assertEquals(RESPONSE_NOT_FOUND, $result['resultCode']);
+  }
+
+  // Login with user ID and wrong password fails
+  public function testWrongPassword() {
+    global $testUserId;
+
+    $data = array('userId'=>$testUserId,
+                  'password'=>'foo');
+    $result = putApi('login.php', $data);
+    $this->assertEquals(RESPONSE_UNAUTHORIZED, $result['resultCode']);
+  }
+
+  // Login with user ID and right password succeeds
+  public function testLoginSuccess() {
+    global $testUserId, $testPassword;
+
+    $data = array('userId'=>$testUserId,
+                  'password'=>$testPassword);
+    $result = putApi('login.php', $data);
+    $this->assertEquals(RESPONSE_SUCCESS, $result['resultCode']);
+  }
+
+  // correct login returns auth token
+  public function testGetAuthToken() {
+    global $testUserId, $testPassword;
+
+    $data = array('userId'=>$testUserId,
+                  'password'=>$testPassword);
+    $result = putApi('login.php', $data);
+    $this->assertArrayHasKey('authId', $result);
+  }
+}
+
+?>
