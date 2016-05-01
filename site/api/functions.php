4213375301ef2f7aadc37f5560d15cf8356665d6
Eric Grivel <github-com@lumenssolutions.com>
2016-05-01 19:54:35 -0400

    externalized functions used in api, so api files can be included

---
+++ site/api/functions.php
@@ -0,0 +1,31 @@
+<?php
+
+function fillCommentItem($item, $object) {
+   $item['tripId'] = $object->getTripId();
+   $item['commentId'] = $object->getCommentId();
+   $item['created'] = $object->getCreated();
+   $item['updated'] = $object->getUpdated();
+   $item['userId'] = $object->getUserId();
+   $item['referenceId'] = $object->getReferenceId();
+   $item['commentText'] = $object->getCommentText();
+   $item['deleted'] = $object->getDeleted();
+
+   return $item;
+}
+
+function fillMediaItem($item, $object) {
+   $item['tripId'] = $object->getTripId();
+   $item['mediaId'] = $object->getMediaId();
+   $item['created'] = $object->getCreated();
+   $item['updated'] = $object->getUpdated();
+   $item['type'] = $object->getType();
+   $item['caption'] = $object->getCaption();
+   $item['timestamp'] = $object->getTimestamp();
+   $item['location'] = $object->getLocation();
+   $item['width'] = $object->getWidth();
+   $item['height'] = $object->getHeight();
+   $item['deleted'] = $object->getDeleted();
+   return $item;
+}
+
+?>
