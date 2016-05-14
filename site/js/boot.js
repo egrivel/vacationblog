bff1d642c2108634fc43bc946782214235aad048
Eric Grivel <github-com@lumenssolutions.com>
2016-05-14 07:35:03 -0400

    store mixin performance improvement

--- site/js/boot.js
+++ site/js/boot.js
@@ -46879,6 +46879,9 @@ function StoreMixin() {
     },
 
     componentDidMount: function() {
+      this._totalChangeEvents = 0;
+      this._totalRendersSaved = 0;
+
       var i;
       for (i = 0; i < this.stores.length; i++) {
         this.stores[i].addChangeListener(this._onChange);
@@ -46889,10 +46892,39 @@ function StoreMixin() {
       for (var i = 0; i < this.stores.length; i++) {
         this.stores[i].removeChangeListener(this._onChange);
       }
+      // console.log('Saved rendering ' + this._totalRendersSaved + ' out of ' +
+      //   this._totalChangeEvents + ' change events');
+    },
+
+    _onEndTimer: function() {
+      if (this._changeCount) {
+        // there was at least one change event that happened while the timer
+        // was running, so there is a need to re-render
+        this.setState(this._getStateFromStores());
+        // we saved rendering all but the last change event
+        this._totalRendersSaved += this._changeCount - 1;
+      }
+      delete this._changeCount;
+    },
+
+    _onChangeOriginal: function() {
+      this.setState(this._getStateFromStores());
     },
 
     _onChange: function() {
-      this.setState(this._getStateFromStores());
+      this._totalChangeEvents++;
+      if (this._changeCount === undefined) {
+        // not on a timer yet. Re-render, but then set a timer to capture all
+        // subsequent change events that would cause a rerender as well and
+        // combine them into a single re-render
+        this._changeCount = 0;
+        this.setState(this._getStateFromStores());
+        setTimeout(this._onEndTimer, 100);
+      } else {
+        // already on a timer to re-render the component, so hold off until
+        // the timer expires
+        this._changeCount++;
+      }
     }
   };
   return Mixin;
