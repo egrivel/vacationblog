91afcaa659be5f4de633bf43d6a9a4ccecd10b37
Eric Grivel <github-com@lumenssolutions.com>
2016-05-19 20:59:11 -0400

    Update unit test for action utils

--- test/actions/test-utils.js
+++ test/actions/test-utils.js
@@ -55,4 +55,41 @@ describe('action utils', function() {
       expect(callback.callCount).to.be.equal(0);
     });
   });
+
+  describe('#postAsync', function() {
+    // TODO: add tests for the data being properly posted
+    var testData;
+
+    beforeEach(function() {
+      testData = {
+        field1: 'value 1',
+        field2: 'value 1'
+      };
+    });
+
+    it('invokes the URL', function() {
+      utils.postAsync(testUrl, testData, callback);
+      expect(requests.length).to.be.equal(1);
+      expect(requests[0].url).to.be.equal(testUrl);
+    });
+
+    it('invokes the callback on success', function() {
+      utils.postAsync(testUrl, testData, callback);
+      expect(requests.length).to.be.equal(1);
+      requests[0].respond(200,
+                          {'Content-Type': 'text/plain'},
+                          testResponse);
+      expect(callback.callCount).to.be.equal(1);
+      expect(callback.firstCall.args[0]).to.be.equal(testResponse);
+    });
+
+    it('does not invoke the callback on failure', function() {
+      utils.postAsync(testUrl, testData, callback);
+      expect(requests.length).to.be.equal(1);
+      requests[0].respond(500,
+                          {'Content-Type': 'text/plain'},
+                          testResponse);
+      expect(callback.callCount).to.be.equal(0);
+    });
+  });
 });
