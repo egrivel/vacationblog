585f39c6cc4065384cf729b51e4626a242822257
Eric Grivel <github-com@lumenssolutions.com>
2016-05-21 13:23:14 -0400

    Can use <Enter> to submit login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,11 +18,17 @@ var Login = React.createClass({
     };
   },
 
-  _onLogin: function() {
+  _onLogin: function(event) {
     var username = this.state.username;
     var password = this.state.password;
     LoginAction.doLogin(username, password);
     this.props.onClose();
+    event.preventDefault();
+    event.stopPropagation();
+  },
+
+  componentDidMount: function() {
+    this.refs.username.focus();
   },
 
   _updateField: function(event) {
@@ -35,13 +41,13 @@ var Login = React.createClass({
 
   render: function() {
     return (
-      <div className="form login">
+      <form className="form login" onSubmit={this._onLogin}>
         <p>
           Please enter your user name and password to log into the site.
         </p>
         <div className="input-element">
           <label>User name:</label>
-          <input type="text" name="username" id="username"
+          <input type="text" ref="username" name="username" id="username"
             value={this.state.username} onChange={this._updateField}/>
         </div>
         <div className="input-element">
@@ -53,7 +59,7 @@ var Login = React.createClass({
           <input type="submit" value="Login" onClick={this._onLogin}/>
           <input type="submit" value="Cancel" onClick={this.props.onClose} />
         </div>
-      </div>
+      </form>
     );
   }
 });
