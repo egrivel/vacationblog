dfaf45d28a95f0319fe50925f88dae68d3c7bbb2
Eric Grivel <github-com@lumenssolutions.com>
2018-04-22 12:53:05 -0400

    added appicon and policy and terms page

---
+++ site/policy-terms.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Vacation Blog Privacy Policy and Terms of Service</title>
+</head>
+
+<body>
+  <h1>Vacation Blog Privacy Policy and Terms of Service</h1>
+
+  <p>The vacation blog is the mechanism through which Eric Grivel
+  and Nicoline Smits share their vacation experiences.</p>
+
+  <p>The vacation blog can be browsed by anyone without providing
+    any personal information, although your IP address may get logged
+    when you browse this site.</p>
+
+  <p>If you choose to login to the vacation blog, you will be asked
+    to provide your name and email address. Your name will be displayed
+    when you interact with the site. Your email address will be used to
+    reset your password if you happen to lose that, and to receive
+    occasional updates (although you can opt out of the latter).</p>
+
+  <p>If you choose to login through facebook, your name and email
+    will be stored (you can opt out of providing your facebook
+    email). Your name will be displayed when you interact with the site.
+    You can opt in to receiving email updates by going to the Preferences
+    section of the site.</p>
+
+  <p>All the comments you post on the site will be publicly visible
+  to all users, whether they are logged in or not.</p>
+
+  <p>You can choose to end your account by removing the
+    <em>Vacation Blog</em> app from facebook, or contacting
+    <em>vacationblog@grivel.net</em> if you registered directly. Any
+    comments you previously made will remain visible associated with
+    your name. Please contact <em>vacationblog@grivel.net</em> if
+    you have a concern with this.</p>
+
+  <p>This website is provided as-is, without any warranty of anything.
+    Although we take reasonable precautions with regard to securing
+    your data, we cannot absolutely guarantee the safety of the
+    information. If this concerns you, please do not create an account
+    on this site.</p>
+
+  <p>Please contact <em>vacationblog@grivel.net</em> with any questions
+    or concerns you may have.</p>
+
+</body>
+</html>
