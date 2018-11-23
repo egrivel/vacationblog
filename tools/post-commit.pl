8d24e785d548cc6dc12a14c9ea4ed8851ad2149f
Eric Grivel <github-com@lumenssolutions.com>
2018-11-22 19:05:57 -0500

    Adding post-commit

---
+++ tools/post-commit.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+
+# This script is called by the GIT server whenever something is pushed
+# the the 'master' branch. It will get a list of the files that have
+# changed.
+
+print "post-update.pl started\n";
+
+chdir("/mnt/data/httpd/html/projects/vacationdir");
+# Get the updates from git
+system("GIT_DIR=.git; git pull");
+$do_rebuild = 0;
+while ($arg = shift) {
+    if ($arg =~ /^src\//) {
+        # Need to re-build React app
+        $do_rebuild = 1;
+    }
+}
+
+if ($do_rebuild) {
+  system("npm run browserify");
+}
+print "post-update.pl done\n";
